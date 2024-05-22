758543aef80c3b3568a20b0dff20202c796a4819
Mike Vink <mike@pionative.com>
2024-05-22 08:58:05 +0200

    24 and 25

--- 3/6_control/24_while_loop.c
+++ 3/6_control/24_while_loop.c
@@ -11,10 +11,10 @@
 
 short loop_while(short a, short b)
 {
-    short result = ;
-    while () {
-        result = ;
-        a = ;
+    short result = 0;
+    while (a > b) {
+        result = result + a*b;
+        a = a - 1;
     }
     return result;
 }
